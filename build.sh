daad3fe62a0f84d22f53af73842a70a9728bc730
Defy <defyasdf@163.com>
2015-05-18 13:58:27 +0800

    修改dojo为标准模板

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Base directory for this entire project
+BASEDIR=$(cd $(dirname $0) && pwd)
+
+# Source directory for unbuilt code
+SRCDIR="$BASEDIR/source"
+
+# Directory containing dojo build utilities
+TOOLSDIR="$SRCDIR/util/buildscripts"
+
+# Destination directory for built code
+DISTDIR="$BASEDIR/build/dist"
+
+# Main application package build configuration
+PROFILE="$BASEDIR/resource/app.profile.js"
+
+# Configuration over. Main application start up!
+
+if [ ! -d "$TOOLSDIR" ]; then
+	echo "Can't find Dojo build tools -- did you initialise submodules? (git submodule update --init --recursive)"
+	exit 1
+fi
+
+echo "Building application with $PROFILE to $DISTDIR."
+
+echo -n "Cleaning old files..."
+rm -rf "$DISTDIR"
+echo " Done"
+
+stylus -c "$SRCDIR/app/resources/app.styl"
+"$TOOLSDIR/build.sh" --profile "$PROFILE" --releaseDir "$DISTDIR" $@
+
+cd "$BASEDIR"
+
+# Copy & minify index.html to dist
+#cat "$SRCDIR/index.html" | \
+#perl -pe 's/\/\/.*$//gm;       # Strip JS comments' |
+#perl -pe 's/\n/ /g;            # Replace newlines with whitespace' |
+#perl -pe 's/<\!--.*?-->//g;    # Strip HTML comments' |
+#perl -pe 's/isDebug: *true,//; # Remove isDebug' |
+#perl -pe 's/\s+/ /g;           # Collapse whitespace' > "$DISTDIR/index.html"
+
+echo "Build complete"
