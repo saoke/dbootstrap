66221925928f28472112a9eff7b423106803e06c
Defy <defyasdf@163.com>
2015-05-24 17:38:12 +0800

    追加几个类

--- source/app/Layout.js
+++ source/app/Layout.js
@@ -1,4 +1,6 @@
 define([ "dojo/parser", "dijit/layout/BorderContainer",
-		"dijit/layout/ContentPane", "dojo/domReady!", "dijit/form/Button" ], function(parser) {
+		"dijit/layout/ContentPane", "dojo/domReady!", "dijit/form/Button",
+		"dijit/layout/AccordionContainer", "dijit/form/TextBox",
+		"dijit/form/SimpleTextarea", "dijit/Editor" ], function(parser) {
 	parser.parse();
 });
