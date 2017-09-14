f638fbd6d12e3dfe2a44279a2656ef70985dfd34
Defy <defyasdf@163.com>
2017-09-14 14:34:22 +0800

    更新最新的配置

--- resource/app.profile.js
+++ resource/app.profile.js
@@ -92,21 +92,34 @@ var profile = {
 	// with dead code removal. A documented list of has-flags in use within the toolkit can be found at
 	// <http://dojotoolkit.org/reference-guide/dojo/has.html>.
 	staticHasFeatures: {
-		// The trace & log APIs are used for debugging the loader, so we do not need them in the build.
-		'dojo-trace-api': false,
-		'dojo-log-api': false,
-
-		// This causes normally private loader data to be exposed for debugging. In a release build, we do not need
-		// that either.
-		'dojo-publish-privates': false,
-
-		// This application is pure AMD, so get rid of the legacy loader.
-		'dojo-sync-loader': false,
-
-		// `dojo-xhr-factory` relies on `dojo-sync-loader`, which we have removed.
-		'dojo-xhr-factory': false,
-
-		// We are not loading tests in production, so we can get rid of some test sniffing code.
-		'dojo-test-sniff': false
+	    "config-deferredInstrumentation": 0,
+	    "config-dojo-loader-catches": 0,
+	    "config-tlmSiblingOfDojo": 0,
+	    "dojo-amd-factory-scan": 0,
+	    "dojo-combo-api": 0,
+	    "dojo-config-api": 1,
+	    "dojo-config-require": 0,
+	    "dojo-debug-messages": 0,
+	    "dojo-dom-ready-api": 1,
+	    "dojo-firebug": 0,
+	    "dojo-guarantee-console": 1,
+	    "dojo-has-api": 1,
+	    "dojo-inject-api": 1,
+	    "dojo-loader": 1,
+	    "dojo-log-api": 0,
+	    "dojo-modulePaths": 0,
+	    "dojo-moduleUrl": 0,
+	    "dojo-publish-privates": 0,
+	    "dojo-requirejs-api": 0,
+	    "dojo-sniff": 1,
+	    "dojo-sync-loader": 0,
+	    "dojo-test-sniff": 0,
+	    "dojo-timeout-api": 0,
+	    "dojo-trace-api": 0,
+	    "dojo-undef-api": 0,
+	    "dojo-v1x-i18n-Api": 1,
+	    "dom": 1,
+	    "host-browser": 1,
+	    "extend-dojo": 1
 	}
 };
